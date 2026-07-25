537f81c7a0c5d1c973321d5a1418f23553a8735e
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-07-25 04:38:08 +0300

    Add booking details for Shalom Products

---
+++ bookings.js
@@ -0,0 +1,11 @@
+const bookingDetailsDatabase = {
+
+  "SHALOM PRODUCTS": `
+    Product Category: Natural Food Products
+    Availability: In Stock
+    Order Type: Wholesale & Retail
+    Delivery: Tanzania Wide
+    Payment: Mobile Money & Bank Transfer
+  `
+
+};
