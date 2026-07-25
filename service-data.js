76002c6ed4562c6b4a069dfba3356b1e251564ec
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-07-25 03:29:07 +0300

    Remove 'enabled' property from payment accounts
    Removed 'enabled' property from payment accounts in service-data.js.

--- service-data.js
+++ service-data.js
@@ -14,38 +14,33 @@ const serviceDatabase = {
       Product Category : Natural Food Products<br><br>
       Availability : In Stock<br><br>
       Order Type : Wholesale & Retail<br><br>
-      Delivery : Tanzania Wide<br><br>
+      Delivery : Tanzania Wide
       Payment : Mobile Money & Bank Transfer
         `,
 
       paymentAccounts: {
 
       mpesa: {
-        enabled: true,
         number: "07XXXXXXXX",
         accountName: "SHALOM PRODUCTS"
       },
 
       airtel: {
-        enabled: true,
         number: "07",
         accountName: "SHALOM PRODUCTS"
       },
 
       mixx: {
-        enabled: true,
         number: "08",
         accountName: "SHALOM PRODUCTS"
       },
 
       nmb: {
-        enabled: true,
         accountName: "SHALOM PRODUCTS",
         accountNumber: "06"
       },
 
       crdb: {
-        enabled: true,
         accountName: "SHALOM PRODUCTS",
         accountNumber: "09"
       }
