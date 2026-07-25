7e2f7eb74daaba7ec76717ac396243297ae2f482
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-07-25 04:33:59 +0300

    Remove bookingDetails from service-data.js
    Removed booking details from service-data.js.

--- service-data.js
+++ service-data.js
@@ -13,14 +13,6 @@ const serviceDatabase = {
       "Natural Products For A Better Life"
     ],
 
-    bookingDetails: `
-      Product Category : Natural Food Products<br><br>
-      Availability : In Stock<br><br>
-      Order Type : Wholesale & Retail<br><br>
-      Delivery : Tanzania Wide
-      Payment : Mobile Money & Bank Transfer
-        `,
-
       paymentAccounts: {
 
       mpesa: {
