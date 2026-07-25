a920850b449879b33f25cdc1d9278eb901ffff43
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-07-25 04:02:12 +0300

    Update accountName assignment to use companyData

--- service.js
+++ service.js
@@ -218,7 +218,7 @@ function showPaymentTab(){
           account.number || "",
 
         accountName:
-          account.accountName || "",
+        companyData.paymentAccountName || "",
 
         accountNumber:
           account.accountNumber || "",
