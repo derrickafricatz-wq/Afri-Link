90d5c236f8efdb14f9ba73c11bde59b90a89ff38
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-08-04 14:50:48 +0300

    Clean up image paths in bg.js
    Removed unused image paths from the images array.

--- bg.js
+++ bg.js
@@ -4,10 +4,7 @@ const campaigns = [
   company: "Announce With Us",
   images: [
     "images/derr.png",
-    "images/s1.png",
-    "images/o1.png",
-    "images/m1.jpg",
-    "images/1.png"
+    "images/s1.png"
   ]
 }
 
