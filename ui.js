c6f8d041e775411a6902ca1010f650757fdea7f5
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-08-05 12:38:15 +0300

    Change Authorization Bearer token in ui.js
    Updated the Authorization header with a new Bearer token.

--- ui.js
+++ ui.js
@@ -2837,7 +2837,7 @@ const response = await fetch(
     mode: "cors",
     headers: {
   "Content-Type": "application/json",
-  "Authorization": "Bearer YOUR_ANON_KEY"
+  "Authorization": "Bearer eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhiZW1rbXZ2Ymt4a251ZHV0aHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0OTUyOTgsImV4cCI6MjA5NTA3MTI5OH0.tKtlJ89jdPyeFSAaQs02Vet3SOl2LVatl4psSG9RxPI"
     }
     body: JSON.stringify({
       request_id: request.id,
