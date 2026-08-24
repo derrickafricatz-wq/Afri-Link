af713b53da6c7f85b8bcd6c9ce91956fb76b4178
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-08-24 03:58:47 +0300

    Reduce Standard fare from 1000 to 100

--- service-data.js
+++ service-data.js
@@ -50,7 +50,7 @@ const serviceDatabase = {
     departure: "Arusha",
     destination: "Dar es Salaam",
     fares: {
-      Standard: 1000,
+      Standard: 100,
       Business: 50000,
       VIP: 70000
     }
