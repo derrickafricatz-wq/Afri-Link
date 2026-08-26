7704c22f6adb1ef3f4ee5229e03397566f84afe1
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-08-26 10:24:14 +0300

    Bump app version to 1.0.7

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Holly Echo - Offline Service Worker
 // ONLINE-FIRST NAVIGATION + CACHE-FIRST RESOURCES
 
-const APP_VERSION = "1.0.6";
+const APP_VERSION = "1.0.7";
 const CACHE_NAME = `voiceofgod-${APP_VERSION}`;
 
 
